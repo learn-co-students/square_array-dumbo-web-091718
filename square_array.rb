888edb2557ab276cb819d225f61b04ff31698440
soohoo05 <tyler.soohoo@gmail.com>
2018-09-05 10:18:01 -0400

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,10 @@
 def square_array(array)
   # your code here
+squaredArray=[]
+counter=0
+  array.each do
+squaredArray[counter]=array[counter]*array[counter]
+counter+=1
+  end
+  return squaredArray
 end
